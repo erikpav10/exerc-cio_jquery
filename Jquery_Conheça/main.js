82b97f19bdabc53964722e724d1f7b7034f97254
Erik F. Pavkovic <erikpav10@yahoo.com.br>
2023-11-17 17:57:44 -0300

    Projeto final

--- Jquery_Conheça/main.js
+++ Jquery_Conheça/main.js
@@ -1,18 +1,35 @@
-$(document).ready(function () {
-    // Adicionar tarefa ao clicar no botão
-    $("#add-button").click(function () {
-        const taskInput = $("#task-input");
-        const taskText = taskInput.val();
-        if (taskText) {
-            const taskList = $("#tasks");
-            const li = $("<li>").text(taskText);
-            taskList.append(li);
-            taskInput.val('');
+$(document).ready(function() {
+    const $taskInput = $("#task-input");
+    const $addButton = $("#add-button");
+    const $taskList = $("#task-list");
+
+    $addButton.on("click", addTask);
+    
+    $taskInput.on("keyup", function(event) {
+        if (event.key === "Enter") {
+        addTask();
         }
     });
 
-    // Alternar a classe "completed" ao clicar em um item da lista
-    $("#tasks").on("click", "li", function () {
+    $taskList.on("click", "li", function() {
         $(this).toggleClass("completed");
     });
+
+    function addTask() {
+        const taskText = $taskInput.val().trim();
+
+        if (taskText !== "") {
+        const existingTask = $taskList.find("li").filter(function() {
+            return $(this).text() === taskText && !$(this).hasClass("completed");
+        });
+        
+        if (existingTask.length > 0) {
+            alert("Tarefa duplicada: Esta tarefa já está na lista como não concluída.");
+        } else {
+            const $listItem = $("<li></li>").text(taskText);
+            $taskList.append($listItem);
+            $taskInput.val("");
+        }
+        }
+    }
 });
